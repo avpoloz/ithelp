8fd83eadadebdcc6bd9ffcf1604be42f1ed64db3
alexander <polozov.dev@gmail.com>
2021-02-21 19:03:23 +0300

    initial commit

---
+++ ITHelpApplication.java
@@ -0,0 +1,13 @@
+package ru.soft.ithelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITHelpApplication.class, args);
+	}
+
+}
